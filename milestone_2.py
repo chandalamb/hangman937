84126e137a43024d0306fab2fcf67451dad95823
chandalamb <154320747+chandalamb@users.noreply.github.com>
2023-12-19 10:09:07 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,6 +1,6 @@
 # The code is importing the 'random' module, which allows us to generate random numbers of make random choices.
 # The code is a game where the user is asked to enter a letter and the code checks if the input is a single alphabetic character. 
-# If it is, the code prints "That is a valiud input.". If not, it prints "Oops! That is not a valid input. Try again."
+# If it is, the code prints "That is a valiud input.". If not, it prints "Oops! That is not a valid input. Please enter a single alphabetical character."
 import random
 
 word_list = ["mango", "banana", "papaya", "grapes", "lychee"]
@@ -9,4 +9,4 @@ guess = input("Enter a letter.\n")
 if ((guess.isalpha() == True) and (len(guess) == 1)):
   print("That is a valid input.")
 else:
-  print("Oops! That is not a valid input. Try again.")
+  print("Oops! That is not a valid input. Please enter a single alphabetical character.")
