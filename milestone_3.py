91a472b88c4c38e293b7c29218ac5ccf0cc6aa05
chandalamb <154320747+chandalamb@users.noreply.github.com>
2023-12-19 10:11:05 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -4,15 +4,6 @@ word_list = ["mango", "banana", "papaya", "grapes", "lychee"]
 word = random.choice(word_list)
 guess = input("Enter a letter.\n")
 
-def check_guess():
-"""
-The function checks if a guess is in a word and prints a corresponding message.
-"""
-if (guess in word):
-  print("Good guess!", guess, "is in the word!")
-else:
-  print("Sorry," guess, "is not in the word. Try again.")
-
 def ask_for_input():
   """
   The function asks the user to enter a single letter and checks if the input is valid.
@@ -25,5 +16,14 @@ def ask_for_input():
     else:
       print("Oops! That is not as valid input. Please enter a single alphabetical character.")
   check_guess()
+  
+def check_guess():
+"""
+The function checks if a guess is in a word and prints a corresponding message.
+"""
+if (guess in word):
+  print("Good guess!", guess, "is in the word!")
+else:
+  print("Sorry," guess, "is not in the word. Try again.")
 
 ask_for_input()
