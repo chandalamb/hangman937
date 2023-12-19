de970619903e7c9a22c55075f1621ab330bb238e
chandalamb <154320747+chandalamb@users.noreply.github.com>
2023-12-19 11:05:37 +0000

    Create milestone_4.py

---
+++ milestone_4.py
@@ -0,0 +1,52 @@
+import random
+word_list = 
+num_lives = 5
+
+# The Hangman class initializes a game of Hangman with a word list and a specified number of lives
+# It keeps track of the word to be guessed, the guessed letters, and the number of letters in the word
+class Hangman():
+  '''
+  A Hangman Game that asks the user for a letter and checks if it is in the word.
+  It starts with a default number of lives and a random word from the word_list.
+
+  Parameters:
+  ------------
+  word_list: list
+    List of words to be used in the game
+  num_lives: int
+    Number of lives the player has
+
+  Attributes:
+  ------------
+  word: str
+    The word to be guess picked randomly from the word_list
+  word_guessed: list
+    A list of the letters of the word, with '_' for each letter not yet guessed
+    For example, if the word is 'grapes', the work_guessed list would be ['_', '_', '_', '_', '_', '_']
+    If the p[layer guesses 'g' the list would be ['g', '_', '_', '_', '_', '_']
+  num_letters: int
+    The number of unique letters in the word that have not been guessed yet
+  num_lives: int
+    The number of lives the player has
+  list_letters: list
+    A list of the letters that have already been tried
+
+    Methods:
+    check_letter(letter)
+      Checks if the letter is in the word.
+    ask_letter()
+      Asks the user for a letter.
+    '''
+  def_init_(self, word_list, num_lives = 5):
+    self.word_list = word_list
+    self.num_lives = num_lives
+    self.word = random.choice(self.word_list)
+    self.word_guessed = ["_" for char in self.word]
+    self.list_of_guesses = []
+    self.num_letters = len(set(self.word))
+    print("The mystery word has,", self.num_letters, "characters")
+    print(self.word_guessed)
+    pass
+
+
+  
